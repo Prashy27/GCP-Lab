44d9ae7ebb69fb4939fddf56970136a9a2e8e7de
Prashaanth <30998564+Prashy27@users.noreply.github.com>
2020-02-06 23:06:24 +0100

    create ansible inventory dynamic

---
+++ create-inventory.tf
@@ -0,0 +1,23 @@
+data "template_file" "inventory_gcp"{
+    template = "${file("${path.module}/inventory_gcp")}"
+    depends_on = ["google_compute_instance.kubemaster",
+                  "google_compute_instance.kubenode01",
+                  "google_compute_instance.kubenode02",
+                  "google_compute_network.kube_vpc",
+                  "google_compute_subnetwork.kube_public_subnet"
+                ]
+    
+    vars {
+        master_ip = "${google_compute_instance.kubemaster.network_interface.*.access_config.*.nat_ip}"
+        worker_ip = "${google_compute_instance.kubenode02.network_interface.*.access_config.*.nat_ip}"
+    }
+}
+
+resource "null_resource" "inventory_gcp" {
+    triggers {
+        template_rendered = "data.template_file.inventory_gcp.rendered"
+    }
+    provisioner "local-exec"{
+        command = "echo '${data.template_file.inventory_gcp.rendered}' > inventory_gcp"
+    }
+}
